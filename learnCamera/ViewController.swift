7b599bc43b7173fe97c650534d48df15edf4cafa
Bijiabo <bijiabo@gmail.com>
2015-12-13 23:27:13 +0800

    add take photo function

--- learnCamera/ViewController.swift
+++ learnCamera/ViewController.swift
@@ -5,9 +5,11 @@
 //  Created by huchunbo on 15/12/12.
 //  Copyright © 2015年 Bijiabo. All rights reserved.
 //
-
+import Foundation
 import UIKit
 import AVFoundation
+import ImageIO
+import CoreGraphics
 
 class ViewController: UIViewController {
 
@@ -15,6 +17,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var panelView: UIView!
     @IBOutlet weak var captureButton: UIButton!
     @IBOutlet weak var switchCameraButton: UIButton!
+    @IBOutlet weak var takePhotoButton: UIButton!
     
     var session: AVCaptureSession!
     
@@ -81,6 +84,7 @@ class ViewController: UIViewController {
         }
         
         // add outputs
+        // add movie output
         let movieOutput: AVCaptureMovieFileOutput = AVCaptureMovieFileOutput()
         let maxDuration: CMTime = CMTimeMake(1000*5, 1000)
         movieOutput.maxRecordedDuration = maxDuration
@@ -88,6 +92,15 @@ class ViewController: UIViewController {
             session.addOutput(movieOutput)
         } else {
             print("capture session does not support add AVCaptureMoviceFileOutput.")
+        }
+        // add still image output
+        let stillImageOutput: AVCaptureStillImageOutput = AVCaptureStillImageOutput()
+        let outputSettings = [AVVideoCodecKey: AVVideoCodecJPEG]
+        stillImageOutput.outputSettings = outputSettings
+        if session.canAddOutput(stillImageOutput) {
+            session.addOutput(stillImageOutput)
+        } else {
+            print("capture session does not support add AVCaptureStillImageOutput.")
         }
         
         session.commitConfiguration()
@@ -350,19 +363,79 @@ class ViewController: UIViewController {
         }
     }
     
+    var currentMovieOutput: AVCaptureMovieFileOutput? {
+        for output in session.outputs {
+            if let output = output as? AVCaptureMovieFileOutput {
+                return output
+            }
+        }
+        return nil
+    }
+    
+    var currentStillImageOutput: AVCaptureStillImageOutput? {
+        for output in session.outputs {
+            if let output = output as? AVCaptureStillImageOutput {
+                return output
+            }
+        }
+        return nil
+    }
+    
     private var recording: Bool = false
     @IBAction func tapCaptureButton(sender: AnyObject) {
         let saveFileURL: NSURL = NSURL(fileURLWithPath: savePath).URLByAppendingPathComponent("\(currentShortDate()).mp4")
         
+        struct buttonColor {
+            static let normal: UIColor = UIColor(red:0.21, green:0.67, blue:0.91, alpha:1)
+            static let active: UIColor = UIColor(red:0.97, green:0.34, blue:0.24, alpha:1)
+        }
+        
+        guard let moviewOutput = currentMovieOutput else {return}
+        
         if !recording {
-            session.outputs[0].startRecordingToOutputFileURL(saveFileURL, recordingDelegate: self)
+            moviewOutput.startRecordingToOutputFileURL(saveFileURL, recordingDelegate: self)
+            captureButton.backgroundColor = buttonColor.active
         } else {
-            session.outputs[0].stopRecording()
+            moviewOutput.stopRecording()
+            captureButton.backgroundColor = buttonColor.normal
         }
         
         recording = !recording
     }
     
+    @IBAction func tapTakePhotoButton(sender: AnyObject) {
+        guard let stillImageOutput = currentStillImageOutput else {return} //TODO: tip user error
+        var videoConnection: AVCaptureConnection? = nil
+        for connection in stillImageOutput.connections as! [AVCaptureConnection] {
+            for port in connection.inputPorts as! [AVCaptureInputPort] {
+                if port.mediaType == AVMediaTypeVideo {
+                    videoConnection = connection
+                    break
+                }
+            }
+            if videoConnection != nil {break}
+        }
+        
+        stillImageOutput.captureStillImageAsynchronouslyFromConnection(videoConnection, completionHandler: {
+            (imageSampleBuffer: CMSampleBuffer!, error: NSError!) -> Void in
+            let exifAttachments = CMGetAttachment(imageSampleBuffer, kCGImagePropertyExifDictionary , nil)
+            if let exifAttachments = exifAttachments {
+                let resultExifAttachments = exifAttachments as! CFDictionaryRef
+                print(resultExifAttachments)
+                
+                let image = UIImage(data: AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(imageSampleBuffer))!
+                UIImageWriteToSavedPhotosAlbum(image, self, Selector("savePhotoToAlbumComplete:"), nil)
+                
+            } else {
+                //TODO: tip user error
+            }
+        })
+    }
+    
+    func savePhotoToAlbumComplete(sender: AnyObject) {
+        print("savePhotoToAlbumComplete")
+    }
+
     // MARK: - date function
     
     func currentShortDate() -> String {
@@ -374,15 +447,71 @@ class ViewController: UIViewController {
         return DateInFormat
     }
     
+    // MARK: - data function
+    
+    // Create a UIImage from sample buffer data
+    
+    private func imageFromSampleBuffer(sampleBuffer: CMSampleBufferRef) -> UIImage? {
+        // Get a CMSampleBuffer's Core Video image buffer for the media data
+        guard let imageBuffer: CVImageBufferRef = CMSampleBufferGetImageBuffer(sampleBuffer) else {return nil}
+        
+        // Lock the base address of the pixel buffer
+        CVPixelBufferLockBaseAddress(imageBuffer, 0)
+        
+        // Get the number of bytes per row for the pixel buffer
+        let baseAddress = CVPixelBufferGetBaseAddress(imageBuffer)
+        
+        // Get the number of bytes per row for the pixel buffer
+        let bytesPerRow = CVPixelBufferGetBytesPerRow(imageBuffer)
+        
+        // Get the pixel buffer width and height
+        let width = CVPixelBufferGetWidth(imageBuffer)
+        let height = CVPixelBufferGetHeight(imageBuffer)
+        
+        // Create a device-dependent RGB color space
+        let colorSpace: CGColorSpaceRef = CGColorSpaceCreateDeviceRGB()!
+        
+        // Create a bitmap graphics context with the sample buffer data
+        let context: CGContextRef = CGBitmapContextCreate(baseAddress, width, height, 8,
+            bytesPerRow, colorSpace, CGBitmapInfo.ByteOrder32Little.rawValue | CGImageAlphaInfo.PremultipliedFirst.rawValue)!
+        
+        // Create a Quartz image from the pixel data in the bitmap graphics context
+        let quartzImage: CGImageRef = CGBitmapContextCreateImage(context)!
+        // Unlock the pixel buffer
+        CVPixelBufferUnlockBaseAddress(imageBuffer,0);
+        
+        // Create an image object from the Quartz image
+        let image: UIImage = UIImage(CGImage: quartzImage)
+        
+        return (image)
+        
+    }
 }
 
 extension ViewController: AVCaptureFileOutputRecordingDelegate {
-    func captureOutput(captureOutput: AVCaptureFileOutput!, didFinishRecordingToOutputFileAtURL outputFileURL: NSURL!, fromConnections connections: [AnyObject]!, error: NSError!) {
-        
-    }
     
     func captureOutput(captureOutput: AVCaptureFileOutput!, didStartRecordingToOutputFileAtURL fileURL: NSURL!, fromConnections connections: [AnyObject]!) {
         
+    }
+    
+    func captureOutput(captureOutput: AVCaptureFileOutput!, didFinishRecordingToOutputFileAtURL outputFileURL: NSURL!, fromConnections connections: [AnyObject]!, error: NSError!) {
+        
+        var recordedSuccessfully: Bool = true
+        
+        if let error = error {
+            if Int32(error.code) != noErr {
+                // A problem occurred: Find out if the recording was successful.
+                if let value = error.userInfo[AVErrorRecordingSuccessfullyFinishedKey] {
+                    if let value_boolValue = value.boolValue {
+                        recordedSuccessfully = value_boolValue
+                    }
+                }
+            }
+        }
+        
+        if !recordedSuccessfully {
+            print("record unsuccessful.")
+        }
     }
 }
 
